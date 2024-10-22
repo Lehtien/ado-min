798b72075b307f2c4731343d2062293bef002e58
lehti <leaf.mmo@gmail.com>
2024-10-22 13:06:44 +0900

    x認証

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -55,6 +55,12 @@ export const authOptions: NextAuthOptions = {
       clientId: env.TWITTER_CLIENT_ID,
       clientSecret: env.TWITTER_CLIENT_SECRET,
       version: "2.0",
+      authorization: {
+        url: "https://twitter.com/i/oauth2/authorize",
+        params: {
+          scope: "users.read tweet.read tweet.write offline.access",
+        },
+      },
     }),
     ...(isDevelopment
       ? [
